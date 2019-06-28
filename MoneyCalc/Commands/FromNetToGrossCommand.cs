213889dec210444b5c1a41820774e12c8a0326c9
Aleksandra Kiszka <aleksandra.kiszka91@gmail.cm>
2019-06-28 14:21:20 +0200

    Replace Click with Command behind Button - first version

---
+++ MoneyCalc/Commands/FromNetToGrossCommand.cs
@@ -0,0 +1,63 @@
+﻿using MoneyCalc.BL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MoneyCalc.Commands
+{
+    public class FromNetToGrossCommand : ICommand, INotifyPropertyChanged
+    {
+        public event EventHandler CanExecuteChanged;
+        //public event EventHandler<double> AmountChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly Func<double> getValueFromInputCallback;
+
+        private readonly Action<double> sendOutputCallback;
+
+        private double _amount;
+        public double Amount
+        {
+            get { return _amount; }
+            set
+            {
+                _amount = value;
+                NotifyPropChanged(nameof(Amount));
+            }
+        }
+
+        public double result;
+        
+
+        public FromNetToGrossCommand(Func<double> getValueFromInputCallback, Action<double> sendOutputCallback)
+        {
+            this.getValueFromInputCallback = getValueFromInputCallback;
+            this.sendOutputCallback = sendOutputCallback;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Amount = getValueFromInputCallback();
+            //AmountChanged(this, Amount);
+            double result = Math.Round((Calculations.FromNetToGross(Amount, 23.00, 18.00)), 2);
+            //MessageBox.Show(result.ToString());
+            sendOutputCallback(result);
+            
+        }
+
+        private void NotifyPropChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
--- MoneyCalc/MainWindow.xaml.cs
+++ MoneyCalc/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MoneyCalc.BL;
+using MoneyCalc.Commands;
 using MoneyCalc.Converters;
 using System;
 using System.Collections.Generic;
@@ -22,33 +23,64 @@ namespace MoneyCalc
     /// </summary>
     public partial class MainWindow : Window
     {       
+        public FromNetToGrossCommand CalcFromNetToGrossCmd { get; }
+        //const double vat = 23.00;
+        //const double tax = 18.00;
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
             VAT.ItemsSource = new List<string> {"No VAT" , "5%", "7%", "8%", "22%", "23%"};
             Tax.ItemsSource = new List<string> { "No TAX", "9%", "18%", "32%"};
+
+            CalcFromNetToGrossCmd = new FromNetToGrossCommand(GetValueFromInput, SendOutput);
         }
 
+        private double GetValueFromInput()
+        {            
+            return Double.Parse(txtInput.Text);
+        }
 
-        private void BtnCountFromNet_Click(object sender, RoutedEventArgs e)
+        private void SendOutput(double result)
         {
-            var input = txtInput.Text;
-            double amount;
-            if (Double.TryParse(input, out amount)) 
-            {
-                amount = Double.Parse(input);
-                double vat = ConvertToCountableType.GetCountableValue(VAT.Text);
-                double tax = ConvertToCountableType.GetCountableValue(Tax.Text);
-                double result = Math.Round((Calculations.FromNetToGross(amount, vat, tax)), 2);
-                txtOutput.Text = $"Kwota brutto - {result.ToString()} PLN";
-            }
-            else
-            {
-                MessageBox.Show("Wpisz wartość numeryczną!");
-            }
-
-            
+            txtOutput.Text = $"Kwota brutto - {result.ToString()} PLN";
         }
+        //private void CountFromNet(object sender, RoutedEventArgs e)
+        //{
+        //    var input = txtInput.Text;
+        //    double amount;
+        //    if (Double.TryParse(input, out amount))
+        //    {
+        //        amount = Double.Parse(input);
+        //        double vat = 23.00;
+        //        double tax = 18.00;
+        //        double result = Math.Round((Calculations.FromNetToGross(amount, vat, tax)), 2);
+        //        txtOutput.Text = $"Kwota brutto - {result.ToString()} PLN";
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Wpisz wartość numeryczną!");
+        //    }
+        //}
+
+        //private void BtnCountFromNet_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var input = txtInput.Text;
+        //    double amount;
+        //    if (Double.TryParse(input, out amount)) 
+        //    {
+        //        amount = Double.Parse(input);
+        //        double vat = ConvertToCountableType.GetCountableValue(VAT.Text);
+        //        double tax = ConvertToCountableType.GetCountableValue(Tax.Text);
+        //        double result = Math.Round((Calculations.FromNetToGross(amount, vat, tax)), 2);
+        //        txtOutput.Text = $"Kwota brutto - {result.ToString()} PLN";
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Wpisz wartość numeryczną!");
+        //    }            
+        //}
 
         private void BtnCountFromGross_Click(object sender, RoutedEventArgs e)
         {
